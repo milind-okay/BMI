9a765883c5c8893153745bc6b77083c6eb474d05
milind-okay <milind0359@gmail.com>
2016-03-31 18:15:10 +0530

    look improvement

--- app/src/main/java/com/wordpress/milindkrohit/bmi/BMIInput.java
+++ app/src/main/java/com/wordpress/milindkrohit/bmi/BMIInput.java
@@ -3,22 +3,18 @@ package com.wordpress.milindkrohit.bmi;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Calendar;
 
 
 /**
@@ -36,7 +32,10 @@ public class BMIInput extends DialogFragment {
     boolean iconType,buttonType;
     EditText mAge,mHeight_ft,mWeight,mHeight_in;
     private Button button;
-    private String age_input,weight_input,height_ft,height_in;
+    private ImageButton img_buttonM,img_buttonF;
+    public boolean ismale=true;
+    public String age_input="",weight_input="",height_ft="",height_in="";
+    public String gender;
 
     @Override
     public void onAttach(Activity activity) {
@@ -64,46 +63,39 @@ public class BMIInput extends DialogFragment {
 
         AlertDialog.Builder builder = new  AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-
         if(m==1) {
             view = inflater.inflate(R.layout.bim_inputs, null);
             // set prompts.xml to alertdialog builder
-
             button = (Button)view.findViewById(R.id.bmiSave);
+            img_buttonM=(ImageButton)view.findViewById(R.id.maleButton);
+            img_buttonF=(ImageButton)view.findViewById(R.id.femaleButton);
+
+            img_buttonM.setOnClickListener(imgButtonHandlerM);
+            img_buttonF.setOnClickListener(imgButtonHandlerF);
+
             final EditText mAge = (EditText) view
                     .findViewById(R.id.bimAgeInput);
             final EditText mHeight_ft = (EditText)view.findViewById(R.id.bmiHeightInput_ft);
             final EditText mHeight_in = (EditText)view.findViewById(R.id.bmiHeightInput_in);
             final EditText mWeight=(EditText)view.findViewById(R.id.bmiWeightInput);
 
-           // builder.setView(view);
-            // set dialog message
-            builder
-                    .setCancelable(true)
-                    .setPositiveButton("Save",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog,int id) {
-
-                                    //  result.setText(userInput.getText());
-                                    age_input = mAge.getText().toString();
-                                    weight_input=mWeight.getText().toString();
-                                    height_ft=mHeight_ft.getText().toString();
-                                    height_in=mHeight_in.getText().toString();
-                                }
-                            })
-                    .setNegativeButton("Cancel",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog,int id) {
-                                    dialog.cancel();
-                                }
-                            });
-
-
 
             builder.setView(view);
             dialog = builder.create();
-           // save(view);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
 
+
+                    age_input = mAge.getText().toString();
+                    weight_input = mWeight.getText().toString();
+                    height_ft = mHeight_ft.getText().toString();
+                    height_in = mHeight_in.getText().toString();
+                    save(view);
+                    dialog.dismiss();
+
+                }
+            });
 
         }else if(m==2){
             view = inflater.inflate(R.layout.bmi_table, null);
@@ -159,6 +151,61 @@ public class BMIInput extends DialogFragment {
 
         return dialog;
     }
+    public void save(View view) {
+        SharedPreferences sharedPreferences=getActivity().getSharedPreferences("MyData", getActivity().MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Age", age_input);
+        editor.putString("weight",weight_input);
+        editor.putString("height_ft",height_ft);
+        editor.putString("height_in", height_in);
+        if(ismale)
+        {
+            editor.putString("gender","male");
+            gender="male";
+        }
+
+
+        else
+        {
+            editor.putString("gender","female");
+            gender="female";
+        }
+
+
+        editor.commit();
+        //Toast.makeText(getActivity(), "Data Saved Succesfully ", Toast.LENGTH_LONG).show();
+    }
+
+    View.OnClickListener imgButtonHandlerM = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            if(!ismale)
+            {
+                img_buttonM.setBackgroundResource(R.drawable.male_blue);
+                ismale=true;
+                img_buttonF.setBackgroundResource(R.drawable.female_light);
+            }
+
+
+
+        }
+    };
+    View.OnClickListener imgButtonHandlerF = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            if(ismale)
+            {
+                img_buttonF.setBackgroundResource(R.drawable.female_blue);
+                ismale=false;
+                img_buttonM.setBackgroundResource(R.drawable.male_light);
+            }
+
+
+
+        }
+    };
+
+
    /* @TargetApi(Build.VERSION_CODES.M)
     public void save(View view)
     {
@@ -178,9 +225,17 @@ public class BMIInput extends DialogFragment {
         return inflater.inflate(R.layout.bim_inputs,null);
 
     }*/
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        if(m==1)
+            dialogComm.showinput(age_input.toString(),weight_input.toString(),height_ft.toString(),height_in.toString(),gender.toString());
+    }
 }
 
 
 interface DialogComm{
     void DialogButtonAction(int a);
+    void showinput(String age,String weight,String ft,String in,String sex);
 }
--- app/src/main/java/com/wordpress/milindkrohit/bmi/MainBMI.java
+++ app/src/main/java/com/wordpress/milindkrohit/bmi/MainBMI.java
@@ -1,35 +1,44 @@
 package com.wordpress.milindkrohit.bmi;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainBMI extends AppCompatActivity implements DialogComm{
+public class MainBMI extends AppCompatActivity implements DialogComm {
+    Animation animRotate;
     ImageView mneedle;
     Button mbuttonok;
-    double mAge,mWeight,mHeight;
+    double  mWeight, mHeight, age, heightFt, heightIn;
     float mNeedleAngle;
-
+    private String age_input = "", height_input_ft = "", height_input_in = "", weight_input = "";
+    private final String Default = "N/A";
+    TextView ageTextView, weightTextView, heightTextViewIn, heightTextViewFt,text_2_part;
+    ImageButton risk_button, target_button, info_button,edit_button;
+    ImageView imageView1,imageView2;
+    String weight,height_in,height_ft,gender,mAge;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +46,6 @@ public class MainBMI extends AppCompatActivity implements DialogComm{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         init();
-
         Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.needle);
         // Getting width & height of the given image.
         int w = bmp.getWidth();
@@ -50,29 +58,125 @@ public class MainBMI extends AppCompatActivity implements DialogComm{
         Bitmap result = Bitmap.createBitmap(125, 125, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(result);
         pcc.draw(canvas);
-      // animRotate = AnimationUtils.loadAnimation(getApplicationContext(),
-            //    R.anim.rotate);
+        // animRotate = AnimationUtils.loadAnimation(getApplicationContext(),
+        //    R.anim.rotate);
         l1.addView(pcc);
-        // start the animation
-       // needle.startAnimation(animRotate);
+
+        // getting id for the buttons...
 
 
+        mneedle.setImageBitmap(bmp);
 
-        mbuttonok.setOnClickListener(new View.OnClickListener() {
+        // checking for the sharedPreferences...
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        mAge = sharedPreferences.getString("Age", Default);
+        weight = sharedPreferences.getString("weight", Default);
+        height_ft = sharedPreferences.getString("height_ft", Default);
+        height_in = sharedPreferences.getString("height_in", Default);
+        gender = sharedPreferences.getString("gender", Default);
+
+        if (!(mAge.equals(Default) || weight.equals(Default) || height_in.equals(Default) || height_ft.equals(Default) || gender.equals(Default))) {
+
+            // convert string to doubles.....
+            // check for the inch measurement....
+            try {
+                // Make use of autoboxing.  It's also easier to read.
+                age = Double.parseDouble(mAge);
+                mWeight = Double.parseDouble(weight);
+                heightFt = Double.parseDouble(height_ft);
+                heightIn = Double.parseDouble(height_in);
+                mHeight = 0.3048 * (heightFt + heightIn);
+            } catch (NumberFormatException e) {
+                // p did not contain a valid double
+            }
+            display();
+            mNeedleAngle = (float) (mWeight / (mHeight * mHeight));
+            turn(mNeedleAngle * 6);
+            //showMyDialog(4);
+            // display the calcuated value ideal weight...........
+
+
+        } else {
+
+            showMyDialog(1);
+            sharedPreferences = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+            mAge = sharedPreferences.getString("Age", Default);
+            weight = sharedPreferences.getString("weight", Default);
+            height_ft = sharedPreferences.getString("height_ft", Default);
+            height_in = sharedPreferences.getString("height_in", Default);
+            gender = sharedPreferences.getString("gender", Default);
+            try {
+                // Make use of autoboxing.  It's also easier to read.
+                age = Double.parseDouble(mAge);
+                mWeight = Double.parseDouble(weight);
+                heightFt = Double.parseDouble(height_ft);
+                heightIn = Double.parseDouble(height_in);
+                mHeight = 0.3048 * (heightFt + heightIn);
+
+            }catch (NumberFormatException e) {
+                // p did not contain a valid double
+            }
+            display();
+        }
+        if(gender=="male")
+        {
+            imageView2.setBackgroundResource(R.drawable.female_light);
+            imageView1.setBackgroundResource(R.drawable.male_blue);
+        }
+        else if(gender=="female")
+        {
+            imageView2.setBackgroundResource(R.drawable.female_blue);
+            imageView1.setBackgroundResource(R.drawable.male_light);
+        }
+
+        edit_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mNeedleAngle = (float) (mWeight / (mHeight * mHeight)) - 15;
-                turn(mNeedleAngle * 6);
-                //showMyDialog(4);
+                showMyDialog(1);
+
             }
         });
-        showMyDialog(1);
 
-
+        info_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showMyDialog(2);
+            }
+        });
+        target_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showMyDialog(4);
+            }
+        });
+        risk_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showMyDialog(3);
+            }
+        });
     }
-    public void init(){
+    public void display() {
+        ageTextView.setText(mAge);
+        weightTextView.setText(weight);
+        heightTextViewIn.setText(height_in);
+        heightTextViewFt.setText(height_ft);
+        String s=String.format("%.2f",mHeight);   // showing the ideal weight.........
+        text_2_part.setText(s);
+    }
+    public void init() {
 
-        mbuttonok = (Button) findViewById(R.id.buttonok);
-        mneedle = (ImageView) findViewById(R.id.needle );
+        //mbuttonok = (Button) findViewById(R.id.buttonok);
+        mneedle = (ImageView) findViewById(R.id.needle);
+        risk_button = (ImageButton) findViewById(R.id.risk_button);
+        info_button = (ImageButton) findViewById(R.id.info_button);
+        target_button = (ImageButton) findViewById(R.id.target_button);
+        edit_button=(ImageButton)findViewById(R.id.editImage);
+        imageView1=(ImageView)findViewById(R.id.imageView1);
+        imageView2=(ImageView)findViewById(R.id.imageView2);
+
+        ageTextView = (TextView) findViewById(R.id.age_display);
+        heightTextViewFt = (TextView) findViewById(R.id.height_ft_display);
+        heightTextViewIn = (TextView) findViewById(R.id.height_in_display);
+        weightTextView = (TextView) findViewById(R.id.weight_display);
+        text_2_part=(TextView)findViewById(R.id.text_2_part);
         mWeight = 60;
         mHeight = 1.54;
     }
@@ -95,34 +199,63 @@ public class MainBMI extends AppCompatActivity implements DialogComm{
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
-    public void turn(float degrees)
-    {
 
-
-        RotateAnimation anim = new RotateAnimation(0, degrees,Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,0.5f);
+    public void turn(float degrees) {
+        RotateAnimation anim = new RotateAnimation(0, degrees, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         anim.setInterpolator(new LinearInterpolator());
-        anim.setDuration(300);
+        anim.setDuration(900);
         anim.setFillEnabled(true);
         anim.setFillAfter(true);
-       mneedle.startAnimation(anim);
-
+        mneedle.startAnimation(anim);
     }
 
     @Override
     public void DialogButtonAction(int a) {
 
     }
+
+    @Override
+    public void showinput(String age, String input_weight, String ft, String in, String sex) {
+        mAge = age;
+        weight=input_weight;
+        height_in=in;
+        height_ft=ft;
+        gender=sex;
+        if(gender=="male")
+        {
+            imageView2.setBackgroundResource(R.drawable.female_light);
+            imageView1.setBackgroundResource(R.drawable.male_blue);
+        }
+        else if(gender=="female")
+        {
+            imageView2.setBackgroundResource(R.drawable.female_blue);
+            imageView1.setBackgroundResource(R.drawable.male_light);
+        }
+        display();
+        try {
+
+            mWeight = Double.parseDouble(weight);
+            heightFt = Double.parseDouble(height_ft);
+            heightIn = Double.parseDouble(height_in);
+            mHeight = 0.3048 * (heightFt + heightIn);
+
+        }catch (NumberFormatException e) {
+            // p did not contain a valid double
+        }
+        mNeedleAngle = (float) (mWeight / (mHeight * mHeight));
+        turn(mNeedleAngle * 6);
+    }
+
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void showMyDialog( int dNum){
-        BMIInput dialogAlert= new BMIInput();
-       dialogAlert.setDialogType(dNum);
-        if(dNum == 2){
-            dialogAlert.setBmiTable("<8","2-3","2-3","2-3","2-3");
-        }else if(dNum == 4){
-            dialogAlert.setTip("5.3","2-3");
+    public void showMyDialog(int dNum) {
+        BMIInput dialogAlert = new BMIInput();
+        dialogAlert.setDialogType(dNum);
+        if (dNum == 2) {
+            dialogAlert.setBmiTable("<8", "2-3", "2-3", "2-3", "2-3");
+        } else if (dNum == 4) {
+            dialogAlert.setTip("5.3", "2-3");
         }
         dialogAlert.show(getFragmentManager(), "gameAlert");
     }
